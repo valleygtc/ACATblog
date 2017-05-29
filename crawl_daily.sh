0fa605bc9affa38e31845ae93ffb26aa1440e404
valleygtc <545788804@qq.com>
2017-05-29 16:15:10 +0800

    change crawl_daily.sh to fix crontab mail

--- crawl_daily.sh
+++ crawl_daily.sh
@@ -1,3 +1,4 @@
+echo "crawl date: $(date)" >>/var/www/ACATblog/crawl_daily.log
 source /var/www/ACATblog/venv/bin/activate
 source /var/www/ACATblog/aliyun_env.sh
-python3 /var/www/ACATblog/manage.py crawl
+python3 /var/www/ACATblog/manage.py crawl >>/var/www/ACATblog/crawl_daily.log
