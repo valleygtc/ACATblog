22ea2d2dc3bbd2d1ed042585de2a484f79c1c62d
valleygtc <545788804@qq.com>
2017-05-30 15:04:28 +0800

    add shebang in crawl_daily.sh &&add aliyun_deploy.sh

--- crawl_daily.sh
+++ crawl_daily.sh
@@ -1,4 +1,8 @@
-echo "crawl date: $(date)" >>/var/www/ACATblog/crawl_daily.log
+#!/bin/bash
+#With crontab.
+#Crawl articles from blog sites.
+echo "crawl date: $(date)"
 source /var/www/ACATblog/venv/bin/activate
 source /var/www/ACATblog/aliyun_env.sh
-python3 /var/www/ACATblog/manage.py crawl >>/var/www/ACATblog/crawl_daily.log
+python3 /var/www/ACATblog/manage.py crawl
+deactivate
