f9a7bf6a5acb54094e37401d0b403e60885401b8
valleygtc <545788804@qq.com>
2017-05-31 20:29:12 +0800

    server side user git&&server .sh fix

--- aliyun_serverstart.sh
+++ aliyun_serverstart.sh
@@ -1,14 +1,14 @@
 #!bin/bash
 
 #purpose:
-#To deploy ACATblog on Aliyun(Ubuntu16.4)
+#ACATblog Server Start on Aliyun(Ubuntu16.4)
 
 #do:
 #1.from aliyun_env.sh export env variables
-#2.use apache2+mod_wsgi delpoy locally for development
+#2.use apache2+mod_wsgi delpoy on aliyun
 
 #usage:
-#as root #:source aliyun_deploy.sh
+#as root #:source aliyun_serverstart.sh
 
 #related file(path:/etc/mod_wsgi-express-80/):
 #config :/etc/mod_wsgi-express-80/httpd.conf
@@ -20,13 +20,10 @@
 source ./venv/bin/activate
 source ./aliyun_env.sh
 
-
-python3 ./manage.py init_db
-
-
 mod_wsgi-express setup-server acatblog.wsgi \
 --host=172.18.85.108 --port=80 \
 --user=www-data --group=www-data \
 --server-root=/etc/mod_wsgi-express-80
+
 /etc/mod_wsgi-express-80/apachectl start
 echo 'apache2 start on port 80'
